472b6a5a9ec93977f9e7ff4819f1f32376e8908a
Martin Walther <it@martin-walther.ch>
2024-11-06 15:00:31 +0100

    Add Modal for single Event

--- public/assets/rotamaster/rotamaster.main.js
+++ public/assets/rotamaster/rotamaster.main.js
@@ -578,6 +578,13 @@ function convertToISOFormat(dateString) {
     return ''; 
 }
 
+function formatDateToShortISOFormat(date) {
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+    return `${day}.${month}.${year}`;
+}
+
 // Export events per person or all events
 function exportCalendarEvents(events, fileName) {
     // Sicherstellen, dass die Eingabe ein Array ist (falls nur ein einzelnes Event übergeben wird)
