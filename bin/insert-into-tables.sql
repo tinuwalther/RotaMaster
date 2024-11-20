95c16ee5d54650dfc02d72697a2f50a0cbff076a
Martin Walther <it@martin-walther.ch>
2024-11-20 14:42:30 +0100

    Create insert-into-tables.sql

---
+++ bin/insert-into-tables.sql
@@ -0,0 +1,26 @@
+-- Inser some absences
+INSERT INTO 'absence' ('name') VALUES
+    ('Blockiert'),
+    ('Ferien'),
+    ('bez. Absenz'),
+    ('unbz. Urlaub'),
+    ('GLZ Kompensation'),
+    ('Aus/Weiterbildung'),
+    ('Militär/ZV/EO'),
+    ('Krankheit'),
+    ('Unfall'),
+    ('Pikett'),
+    ('Pikett-Peer');
+
+-- Insert some person
+INSERT INTO 'person' ('firstname','name') VALUES
+    ('Freddie','Mercury'),
+    ('Ozzy','Osbourne'),
+    ('Robert','Plant'),
+    ('Kurt','Cobain'),
+    ('Alice','Cooper'),
+    ('Ronnie James','Dio'),
+    ('Bruce','Dickinson'),
+    ('Till','Lindemann'),
+    ('Marilyn','Manson'),
+    ('Jim','Morrison');
