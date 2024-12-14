8a35c55615cd02eba01f0c7d6632276095329e61
Martin Walther <it@martin-walther.ch>
2024-12-14 11:58:45 +0100

    Create create-events-view.sql

---
+++ bin/create-events-view.sql
@@ -0,0 +1,20 @@
+-- Create the view for events with email
+CREATE VIEW v_events
+AS 
+SELECT 
+  e.id,
+  e.person,
+  e.type,
+  e.start,
+  e.end,
+  p.login,
+  p.firstname,
+  p.name,
+  p.email,
+  e.created,
+  e.author
+FROM
+  events e
+  INNER JOIN person p ON (p.firstname || " " || p.name) = e.person
+ORDER BY 
+  e.start ASC;
