7a2f4d33f6d96cd57d8a306179c7770dbfc99f82
Martin Walther <it@martin-walther.ch>
2024-12-15 10:53:07 +0100

    Update insert-into-tables.sql

--- bin/insert-into-tables.sql
+++ bin/insert-into-tables.sql
@@ -13,14 +13,14 @@ INSERT INTO 'absence' ('name','author') VALUES
     ('Pikett-Peer','Administrator');
 
 -- Insert some person
-INSERT INTO 'person' ('login','firstname','name','author') VALUES
-    ('fmercury','Freddie','Mercury','Administrator'),
-    ('oosbourne','Ozzy','Osbourne','Administrator'),
-    ('rplant','Robert','Plant','Administrator'),
-    ('kcobain','Kurt','Cobain','Administrator'),
-    ('acooper','Alice','Cooper','Administrator'),
-    ('rjdio','Ronnie James','Dio','Administrator'),
-    ('bdickinso','Bruce','Dickinson','Administrator'),
-    ('tlindeman','Till','Lindemann','Administrator'),
-    ('mmanson','Marilyn','Manson','Administrator'),
-    ('jmorrison','Jim','Morrison','Administrator');
+INSERT INTO 'person' ('login','firstname','name','email','author') VALUES
+    ('fmercury','Freddie','Mercury',LOWER(firstname || '.' || name || '@company.com'),'Administrator'),
+    ('oosbourne','Ozzy','Osbourne',LOWER(firstname || '.' || name || '@company.com'),'Administrator'),
+    ('rplant','Robert','Plant',LOWER(firstname || '.' || name || '@company.com'),'Administrator'),
+    ('kcobain','Kurt','Cobain',LOWER(firstname || '.' || name || '@company.com'),'Administrator'),
+    ('acooper','Alice','Cooper',LOWER(firstname || '.' || name || '@company.com'),'Administrator'),
+    ('rjdio','Ronnie James','Dio',LOWER(firstname || '.' || name || '@company.com'),'Administrator'),
+    ('bdickinso','Bruce','Dickinson',LOWER(firstname || '.' || name || '@company.com'),'Administrator'),
+    ('tlindeman','Till','Lindemann',LOWER(firstname || '.' || name || '@company.com'),'Administrator'),
+    ('mmanson','Marilyn','Manson',LOWER(firstname || '.' || name || '@company.com'),'Administrator'),
+    ('jmorrison','Jim','Morrison',LOWER(firstname || '.' || name || '@company.com'),'Administrator');
