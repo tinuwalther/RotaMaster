442a272eef9b6d4da6c21151454463524e5ff89a
Martin Walther <it@martin-walther.ch>
2025-01-04 16:24:50 +0100

    Add config-file

---
+++ public/assets/rotamaster/rotamaster.config.js
@@ -0,0 +1,41 @@
+/**
+ * Configuration object for the FullCalendar instance.
+ * 
+ * This constant defines the default configuration settings for a FullCalendar instance, 
+ * including the application version, time zone, locale, initial view, toolbar layout, 
+ * button labels, and various display preferences. It centralizes the calendar setup 
+ * to ensure consistency across the application.
+ */
+const calendarConfig = {
+    appVersion: "5.4.0",
+    appPrefix: "PS",
+    opsGenie: true,
+    scheduleName: 'tinu_schedule',
+    rotationName: '2025',
+    timeZone: 'local',
+    locale: 'de-CH',
+    themeSystem: 'standard',
+    initialView: 'multiMonthYear',
+    multiMonthMinWidth: 350,
+    multiMonthMaxColumns: 2,
+    headerToolbar: {
+        left: 'prevYear,prev,today,next,nextYear refreshButton',
+        center: 'title',
+        right: 'multiMonthYear,dayGridMonth,listMonth exportToIcs,filterEvents'
+    },
+    buttonText: {
+        today: 'Heute',
+        year: 'Jahr',
+        month: 'Monat',
+        list: 'Liste'
+    },
+    weekNumbers: true,
+    dayMaxEvents: true,
+    showNonCurrentDates: false,
+    fixedWeekCount: false,
+    weekNumberCalculation: 'ISO',
+    selectable: true,
+    editable: true,
+    displayEventTime: false,
+    navLinks: true
+};
