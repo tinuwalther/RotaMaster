762c4e2412ed9b26cd5397903025647332ddd15e
Martin Walther <it@martin-walther.ch>
2025-02-22 17:37:23 +0100

    Update rotamaster.config.js

--- public/assets/rotamaster/rotamaster.config.js
+++ public/assets/rotamaster/rotamaster.config.js
@@ -7,7 +7,7 @@
  * to ensure consistency across the application.
  */
 const calendarConfig = {
-    appVersion: "5.4.3",
+    appVersion: "5.4.4",
     appPrefix: "PS",
     opsGenie: true,
     scheduleName: 'tinu_schedule',
